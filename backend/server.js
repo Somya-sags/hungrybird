fbf3d208b4f781079f11ba48e314965b69658016
Somya sagar swain <somyasagarswain45@gmail.com>
2026-04-15 17:52:13 +0530

    Fixed port issue

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,8 @@ import Order from "./models/orderSchema.js";
 dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors({
   origin: [
@@ -27,8 +29,8 @@ app.use("/api/auth",authRoutes);
 
 connectDB();
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 
